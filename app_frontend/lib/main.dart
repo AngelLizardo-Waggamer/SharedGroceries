a9499d342b4f1fca6e1cb04892260d8dea920035
Ángel Lizardo <aahlizardo.britanico@gmail.com>
2026-02-27 05:10:24 -0600

    Configurar punto de entrada de la aplicación

--- app_frontend/lib/main.dart
+++ app_frontend/lib/main.dart
@@ -1,28 +1,36 @@
-import 'package:app_frontend/Views/home.dart';
-import 'package:app_frontend/Views/login.dart';
+import 'package:app_frontend/Routes/routes.dart';
+import 'package:app_frontend/Theme/app_theme.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widget_previews.dart';
+import 'package:flutter/services.dart';
 
-void main() {
-  runApp(const MainApp());
+void main() async {
+
+	WidgetsFlutterBinding.ensureInitialized();
+	await SystemChrome.setPreferredOrientations([
+		DeviceOrientation.portraitUp,
+	]);
+
+	runApp(const MainApp());
+}
+
+// Validación inicial para saber si tiene una sesión activa o no.
+bool _isAuthenticated() {
+	// TODO: verificar token/sesión almacenada
+	return false;
 }
 
 class MainApp extends StatelessWidget {
-  @Preview(
-    name: 'MainApp'
-  )
+	const MainApp({super.key});
 
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      initialRoute: '/login',
-
-      routes: {
-        '/': (context) => const HomeView(),
-        '/login': (context) => const LoginView(),
-      },
-    );
-  }
+	@override
+	Widget build(BuildContext context) {
+	
+	return MaterialApp(
+		title: 'Shared Groceries',
+		themeMode: .light,
+		theme: AppTheme.light,
+		initialRoute: _isAuthenticated() ? '/home' : '/login',
+		routes: Routes.routes
+	);
+	}
 }
