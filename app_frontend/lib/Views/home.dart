78b20b8c5e1fdd6522ce0da4eca7647585ce37fc
Ángel Lizardo <aahlizardo.britanico@gmail.com>
2026-02-27 05:10:16 -0600

    Actualizar vistas de home y login

--- app_frontend/lib/Views/home.dart
+++ app_frontend/lib/Views/home.dart
@@ -1,14 +1,82 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widget_previews.dart';
 
 class HomeView extends StatelessWidget {
-  @Preview(
-    name: 'HomeView S23+'
-  )
-  const HomeView({super.key});
+	const HomeView({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+	@override
+	Widget build(BuildContext context) {
+    // TODO: Usar mejor medidas basadas en deviceWidth y Height
+		// double deviceWidth = MediaQuery.of(context).size.width;
+		// double deviceHeight = MediaQuery.of(context).size.height;
+
+		// Mostrar un mensaje de bienvenida al cargar la pantalla justo después del primer frame
+		WidgetsBinding.instance.addPostFrameCallback((_) {
+			_showSnackBar(context, "¡Bienvenido a Shared Groceries!");
+		});
+
+		return Scaffold(
+			appBar: AppBar(
+				title: const Text('Mis listas'),
+				centerTitle: true,
+				leading: const Icon(Icons.local_grocery_store),
+				actions: [PopupMenuButton(itemBuilder: buildPopupMenuItem)],
+			),
+
+			body: FutureBuilder(future: Future.delayed(Duration(seconds: 2)), builder: futureFetchingListsVisualization)
+		);
+	}
+
+	Widget futureFetchingListsVisualization(BuildContext context, snapshot) {
+				if (snapshot.connectionState == ConnectionState.waiting) {
+					return const Center(child: CircularProgressIndicator());
+				} else if (snapshot.hasError) {
+					return Center(
+						child: Text('Error al cargar las listas',
+							style: TextStyle(color: Theme.of(context).colorScheme.onError),
+						)
+					);
+				} else {
+					if (snapshot.data == null || snapshot.data <= 0) {
+						return Padding(
+							padding: const EdgeInsets.only(left: 40, right: 40),
+								child: Expanded(
+									child: Column(
+										mainAxisAlignment: .center,
+										crossAxisAlignment: .center,
+										mainAxisSize: .max,
+										children: [
+											Text("No hay listas disponibles", style: Theme.of(context).textTheme.headlineMedium,),
+											SizedBox(height: 40,),
+											ElevatedButton(onPressed: () {}, child: Text("Crear nueva lista"))
+										],
+									),
+								),
+						);
+					}
+
+					return ListView.builder(itemBuilder: listItemBuilder, itemCount: snapshot.data.length);
+				}
+			}
+
+	// TODO: Reemplazar esto por la visualización de los datos que se obtengan de la API.
+	Widget listItemBuilder(BuildContext context, int index) {
+		return ListTile(
+			title: Text("Lista $index"),
+			subtitle: Text("Descripción de la lista $index"),
+			trailing: Icon(Icons.arrow_forward_ios),
+			onTap: () {},
+		);
+	}
+
+	List<PopupMenuEntry<String>> buildPopupMenuItem(BuildContext context) => [
+		const PopupMenuItem(value: 'family', child: Text('Familia')),
+	];
+
+	void _showSnackBar(BuildContext context, String message) {
+		ScaffoldMessenger.of(context).showSnackBar(
+			SnackBar(content: Text(message), duration: Duration(seconds: 1, milliseconds: 500),),
+		);
+	}
+
+	
 }
--- app_frontend/lib/Views/login.dart
+++ app_frontend/lib/Views/login.dart
@@ -1,15 +1,86 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class LoginView extends StatefulWidget {
-  const LoginView({super.key});
+	const LoginView({super.key});
 
-  @override
-  State<LoginView> createState() => _LoginViewState();
+	@override
+	State<LoginView> createState() => _LoginViewState();
 }
 
 class _LoginViewState extends State<LoginView> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+
+	final TextEditingController _userController = TextEditingController();
+	final TextEditingController _passwordController = TextEditingController();
+
+	@override
+	Widget build(BuildContext context) {
+
+		double deviceWidth = MediaQuery.of(context).size.width;
+		double deviceHeight = MediaQuery.of(context).size.height;
+
+		return GestureDetector(
+			behavior: .opaque,
+			onTap: () => FocusScope.of(context).unfocus(),
+			child: Scaffold(
+				body: Stack(
+					children: [
+						ImageFiltered(
+							imageFilter: ImageFilter.blur(sigmaX: 3, sigmaY: 3),
+							child: Image.asset(
+								'assets/images/fondo_login_registro.png',
+								width: deviceWidth,
+								height: deviceHeight,
+								fit: BoxFit.cover
+							)
+						),
+						Center(
+							child: SizedBox(
+								width: deviceWidth * 0.85,
+								height: deviceHeight * 0.65,
+								child: _buildLoginCard(() => Navigator.of(context).pushReplacementNamed('/home'), () => Navigator.of(context).pushNamed('/register'))
+								),
+							)
+					],
+				)
+			),
+		);
+
+	}
+
+	Card _buildLoginCard(Function() onLoginBtnPressed, Function() onRegisterBtnPressed) {
+		return Card(
+			child: Padding(
+				padding: const EdgeInsets.fromLTRB(20, 12, 20, 12),
+				child: Column(
+					mainAxisSize: .max,
+					mainAxisAlignment: .center,
+					crossAxisAlignment: .start,
+
+					children: [
+						SizedBox(
+							width: .infinity,
+							child: Center(child: Text("Iniciar sesión", style: Theme.of(context).textTheme.headlineMedium)),
+						),
+						SizedBox(height: 40), // TODO: Usar mejor medidas basadas en deviceHeight
+						Text("Usuario", style: Theme.of(context).textTheme.titleLarge),
+						SizedBox(height: 15),
+						TextField(controller: _userController),
+						SizedBox(height: 40),
+						Text("Contraseña", style: Theme.of(context).textTheme.titleLarge),
+						SizedBox(height: 15),
+						TextField(controller: _passwordController, obscureText: true),
+						SizedBox(height: 40),
+						FilledButton(onPressed: onLoginBtnPressed, child: const Text("Ingresar")),
+						SizedBox(height: 15),
+						SizedBox(
+							width: .infinity,
+							child: TextButton(onPressed: onRegisterBtnPressed, child: const Text("Registrarse"))
+						)
+					],
+				)
+			),
+		);
+	}
 }
